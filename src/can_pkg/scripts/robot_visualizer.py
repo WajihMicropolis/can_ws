340c601ebdf1117f6447dd1b8a02eed157bc9d15
Maya <mayahashash00@gmail.com>
2024-06-12 22:13:26 +0300

    create references folder

--- src/can_pkg/scripts/robot_visualizer.py
+++ src/can_pkg/scripts/robot_visualizer.py
@@ -1,20 +1,35 @@
 #!/bin/python3
 
-import rospy
-import rospkg
-from PyQt5 import QtCore, QtWidgets, QtGui
+from PyQt6 import QtCore, QtWidgets, QtGui
 from std_msgs.msg import Int16MultiArray, String, Float32, Bool
 from sensor_msgs.msg import BatteryState
-
-from visualizer_V2 import Ui_Dialog  # Import the auto-generated UI class
+import rospy
+from visualizer_V3 import Ui_Dialog  # Import the auto-generated UI class
 
 class RobotVisualizer(QtWidgets.QDialog, Ui_Dialog):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         rospy.init_node('feedback_visualizer_node', anonymous=True)
-        self.rospack = rospkg.RosPack()
         
+        self.battery = BatteryState()
+        
+        # Subscribers
+        self.motor_speed_sub        = rospy.Subscriber('feedback/motors_speed', Int16MultiArray, self.motor_speed_cb)
+        self.steering_angle_sub     = rospy.Subscriber('feedback/steering_angle', Int16MultiArray, self.steering_angle_cb)
+        self.brake_percentage_sub   = rospy.Subscriber('feedback/brake_percentage', Int16MultiArray, self.brake_percentage_cb)
+        self.ultrasonic_sub         = rospy.Subscriber('feedback/ultrasonic', Int16MultiArray, self.ultrasonic_cb)
+        self.emergency_brake_sub    = rospy.Subscriber('robot/emergency_brake', Bool, self.emergency_brake_cb)
+        self.driving_HC_sub         = rospy.Subscriber('feedback/driving_health_check', Bool, self.driving_HC_cb)
+        self.steering_HC_sub        = rospy.Subscriber('feedback/steering_health_check', Bool, self.steering_HC_cb)
+        self.braking_HC_sub         = rospy.Subscriber('feedback/braking_health_check', Bool, self.braking_HC_cb)
+        self.steering_state_sub     = rospy.Subscriber('feedback/steering_state', String, self.steering_state_cb)
+        self.brake_state_sub        = rospy.Subscriber('feedback/brake_state', String, self.brake_state_cb)
+        self.robot_velocity_sub     = rospy.Subscriber('robot/velocity', Float32, self.robot_velocity_cb)
+        self.robot_steering_sub     = rospy.Subscriber('robot/steering', Float32, self.robot_steering_cb)
+        self.robot_state_sub        = rospy.Subscriber('robot/state', String, self.robot_state_cb)
+        self.battery_sub            = rospy.Subscriber("feedback/battery",BatteryState, lambda msg : setattr(self, 'battery', msg))
+
         # Variables
         self.motors_speed = Int16MultiArray()
         self.steering_angle = Int16MultiArray()
@@ -25,28 +40,16 @@ class RobotVisualizer(QtWidgets.QDialog, Ui_Dialog):
         self.robot_velocity = Float32()
         self.robot_steering = Float32()
         self.emergency_brake = Bool()
-        self.battery = BatteryState()
-        
-        # Subscribers
-        self.motor_speed_sub        = rospy.Subscriber('feedback/motors_speed', Int16MultiArray, self.motor_speed_cb)
-        self.steering_angle_sub     = rospy.Subscriber('feedback/steering_angle', Int16MultiArray, self.steering_angle_cb)
-        self.brake_percentage_sub   = rospy.Subscriber('feedback/brake_percentage', Int16MultiArray, self.brake_percentage_cb)
-        self.ultrasonic_sub         = rospy.Subscriber('feedback/ultrasonic', Int16MultiArray, self.ultrasonic_cb)
-        self.emergency_brake_sub    = rospy.Subscriber('robot/emergency_brake', Bool, self.emergency_brake_cb)
-        self.steering_state_sub     = rospy.Subscriber('feedback/steering_state', String, self.steering_state_cb)
-        self.brake_state_sub        = rospy.Subscriber('feedback/brake_state', String, self.brake_state_cb)
-        self.robot_velocity_sub     = rospy.Subscriber('robot/velocity', Float32, self.robot_velocity_cb)
-        self.robot_steering_sub     = rospy.Subscriber('robot/steering', Float32, self.robot_steering_cb)
-        self.battery_sub            = rospy.Subscriber("feedback/battery",BatteryState, lambda msg : setattr(self, 'battery', msg))
-
+        self.robot_state = String()
+        self.driving_HC = Bool()
+        self.steering_HC = Bool()
+        self.braking_HC = Bool()
         
         # Image paths
-        pkg_path = self.rospack.get_path('can_pkg')
-        package_path = pkg_path+"/images"
-        
+        package_path = "/home/maya/can_ws/src/can_pkg/images"
         self.image_paths = {
-            "speed_logo": package_path + "/speed_visualizer.png",
             "main_picture": package_path + "/M2_robot.png",
+            "speed_logo": package_path + "/speed_visualizer.png",
             "steering_logo": package_path + "/steering_visualizer.png",
             "break_logo": package_path + "/break_visualizer.png",
             "break_error_logo": package_path + "/break_error.png",
@@ -57,7 +60,12 @@ class RobotVisualizer(QtWidgets.QDialog, Ui_Dialog):
             "FOV_down": package_path + "/FOV_down.png",
             "FOV_right": package_path + "/FOV_right.png",
             "FOV_left": package_path + "/FOV_left.png",
-            "battery_logo": package_path + "/battery.png"
+            "battery_logo": package_path + "/battery.png",
+            "connection_0_20": package_path + "/0_20.png",
+            "connection_20_40": package_path + "/20_40.png",
+            "connection_40_60": package_path + "/40_60.png",
+            "connection_60_80": package_path + "/60_80.png",
+            "connection_80_100": package_path + "/80_100.png",
         }
         
         #set style
@@ -79,16 +87,16 @@ class RobotVisualizer(QtWidgets.QDialog, Ui_Dialog):
         
     #*callbacks   
     def motor_speed_cb(self, msg: Int16MultiArray):
-        self.motors_speed.data = msg.data
+        self.motors_speed = msg.data
 
     def steering_angle_cb(self, msg: Int16MultiArray):
-        self.steering_angle.data = msg.data
+        self.steering_angle = msg.data
 
     def brake_percentage_cb(self, msg: Int16MultiArray):
-        self.brake_percentage.data = msg.data
+        self.brake_percentage = msg.data
     
     def ultrasonic_cb(self, msg: Int16MultiArray):
-        self.ultrasonic.data = msg.data
+        self.ultrasonic = msg.data
 
     def steering_state_cb(self, msg: String):
         self.steer_error = msg.data.split(":")
@@ -97,15 +105,26 @@ class RobotVisualizer(QtWidgets.QDialog, Ui_Dialog):
         self.brake_error = msg.data.split(":")
         
     def robot_velocity_cb(self, msg: Float32):
-        self.robot_velocity.data = msg.data
+        self.robot_velocity = msg.data
         
     def robot_steering_cb(self, msg: Float32):
-        self.robot_steering.data = msg.data
+        self.robot_steering = msg.data
         
     def emergency_brake_cb(self, msg:Bool):
-        self.emergency_brake.data = msg.data
+        self.emergency_brake = msg.data
         
-
+    def robot_state_cb(self, msg: String):
+        self.robot_state = msg.data
+        
+    def driving_HC_cb(self, msg: Bool):
+        self.driving_HC = msg.data
+        
+    def steering_HC_cb(self, msg: Bool):
+        self.steering_HC = msg.data
+        
+    def braking_HC_cb(self, msg: Bool):
+        self.braking_HC = msg.data
+        
         
     def set_images(self):
         self.picture.setPixmap(QtGui.QPixmap(self.image_paths["main_picture"]))
@@ -141,34 +160,34 @@ class RobotVisualizer(QtWidgets.QDialog, Ui_Dialog):
         self.BR_FOV.setPixmap(QtGui.QPixmap(self.image_paths["FOV_down"]))
         self.BL_FOV.setPixmap(QtGui.QPixmap(self.image_paths["FOV_down"]))
         self.battery_pic.setPixmap(QtGui.QPixmap(self.image_paths["battery_logo"]))
-    
+        self.connection_pic.setPixmap(QtGui.QPixmap(self.image_paths["connection_80_100"]))
 
     def update_labels(self):
-        if self.motors_speed.data :
-            self.FR_speed_label.setText(f"{self.motors_speed.data[0]} rpm")
-            self.BR_speed_label.setText(f"{self.motors_speed.data[1]} rpm")
-            self.FL_speed_label.setText(f"{self.motors_speed.data[2]} rpm")
-            self.BL_speed_label.setText(f"{self.motors_speed.data[3]} rpm")
+        if self.motors_speed:
+            self.FR_speed_label.setText(f"{self.motors_speed[0]} rpm")
+            self.BR_speed_label.setText(f"{self.motors_speed[1]} rpm")
+            self.FL_speed_label.setText(f"{self.motors_speed[2]} rpm")
+            self.BL_speed_label.setText(f"{self.motors_speed[3]} rpm")
 
-        if self.steering_angle.data:
-            self.FR_steering_label.setText(f"{self.steering_angle.data[0]}°")
-            self.FL_steering_label.setText(f"{self.steering_angle.data[1]}°")
-            self.BR_steering_label.setText(f"{self.steering_angle.data[2]}°")
-            self.BL_steering_label.setText(f"{self.steering_angle.data[3]}°")
+        if self.steering_angle:
+            self.FR_steering_label.setText(f"{self.steering_angle[0]}°")
+            self.FL_steering_label.setText(f"{self.steering_angle[1]}°")
+            self.BR_steering_label.setText(f"{self.steering_angle[2]}°")
+            self.BL_steering_label.setText(f"{self.steering_angle[3]}°")
 
-        if self.brake_percentage.data:
-            self.Fr_break_label.setText(f"{self.brake_percentage.data[0]}%")
-            self.FL_break_label.setText(f"{self.brake_percentage.data[1]}%")
-            self.BR_break_label.setText(f"{self.brake_percentage.data[2]}%")
-            self.BL_break_label.setText(f"{self.brake_percentage.data[3]}%")
+        if self.brake_percentage:
+            self.Fr_break_label.setText(f"{self.brake_percentage[0]}%")
+            self.FL_break_label.setText(f"{self.brake_percentage[1]}%")
+            self.BR_break_label.setText(f"{self.brake_percentage[2]}%")
+            self.BL_break_label.setText(f"{self.brake_percentage[3]}%")
             
-        if self.ultrasonic.data:
-            self.FR_ultrasonic_label.setText(f"{self.ultrasonic.data[0]}")
-            self.FL_ultrasonic_label.setText(f"{self.ultrasonic.data[1]}")
-            self.BR_ultrasonic_label.setText(f"{self.ultrasonic.data[2]}")
-            self.BL_ultrasonic_label.setText(f"{self.ultrasonic.data[3]}")
-            self.R_ultrasonic_label.setText(f"{self.ultrasonic.data[4]}")
-            self.L_ultrasonic_label.setText(f"{self.ultrasonic.data[5]}") 
+        if self.ultrasonic:
+            self.FR_ultrasonic_label.setText(f"{self.ultrasonic[0]}")
+            self.FL_ultrasonic_label.setText(f"{self.ultrasonic[1]}")
+            self.BR_ultrasonic_label.setText(f"{self.ultrasonic[2]}")
+            self.BL_ultrasonic_label.setText(f"{self.ultrasonic[3]}")
+            self.R_ultrasonic_label.setText(f"{self.ultrasonic[4]}")
+            self.L_ultrasonic_label.setText(f"{self.ultrasonic[5]}") 
          
         if self.steer_error:
             self.FR_steering_error_label.setText(self.steer_error[0])
@@ -181,26 +200,90 @@ class RobotVisualizer(QtWidgets.QDialog, Ui_Dialog):
             self.FL_break_error_label.setText(self.brake_error[1])
             self.BR_break_error_label.setText(self.brake_error[2])
             self.BL_break_error_label.setText(self.brake_error[3])
+        
+        if self.robot_state:
+            self.robot_state_label.setText(f"Robot state: {self.robot_state}")
+        
+        if self.robot_velocity:
+            self.control_speed_label.setText(f"{self.robot_velocity} rpm")
             
-        # if self.robot_velocity.data:
-            self.control_speed_label.setText(f"{self.robot_velocity.data} rpm")
+        if self.robot_steering:
+            self.control_steering_label.setText(f"{self.robot_steering}°")
             
-        # if self.robot_steering.data:
-            self.control_steering_label.setText(f"{self.robot_steering.data}°")
-            
-        if self.emergency_brake.data:
-            self.control_break_label.setText("True")
+        if self.emergency_brake:
+            self.control_break_label.setText("⚠")
             self.control_break_label.setStyleSheet("color: red")
             
-        if not self.emergency_brake.data:
-            self.control_break_label.setText("False")
-            self.control_break_label.setStyleSheet("color: white")
+        if not self.emergency_brake:
+            self.control_break_label.setText("☑")
+            self.control_break_label.setStyleSheet("color: green")
             
-        if self.battery.voltage:
-            self.batter_percentage_label.setText(f"{int(self.battery.percentage)} %")
-            self.battery_current_label.setText(f"{round(self.battery.current,1)} A")
-            self.battery_voltage_label.setText(f"{round(self.battery.voltage,1)} V")
+        if self.driving_HC:
+            self.health_check_drive_label.setText("☑")
+            self.health_check_drive_label.setStyleSheet("color: green")
             
+        if not self.driving_HC:
+            self.health_check_drive_label.setText("☒")
+            self.health_check_drive_label.setStyleSheet("color: red")
+                            
+        if self.steering_HC:
+            self.health_check_steer_label.setText("☑")
+            self.health_check_steer_label.setStyleSheet("color: green")
+            
+        if not self.steering_HC:
+            self.health_check_steer_label.setText("☒")
+            self.health_check_steer_label.setStyleSheet("color: red")
+            
+        # if self.braking_HC:
+        #     self.health_check_brake_label.setText("☑")
+        #     self.health_check_brake_label.setStyleSheet("color: green")
+            
+        # if not self.braking_HC:
+        #     self.health_check_brake_label.setText("☒")
+        #     self.health_check_brake_label.setStyleSheet("color: red")
+            
+            
+        if self.battery:
+            self.battery_temp_label.setText(f"{self.battery.temperature}°C")
+            self.battery_current_label.setText(f"{self.battery.current} A")
+            self.battery_voltage_label.setText(f"{self.battery.voltage} V")
+            self.battery_progressbar.setValue(int(self.battery.percentage))
+            if (self.battery.percentage <= 30):
+                self.battery_progressbar.setStyleSheet("""
+                                                        QProgressBar{
+                                                            border: 2px solid grey;
+                                                            border-radius: 5px;
+                                                            text-align: center
+                                                        }
+
+                                                        QProgressBar::chunk {
+                                                            background-color: red;
+                                                        }
+                                                        """)
+            elif (self.battery.percentage >= 60):
+                self.battery_progressbar.setStyleSheet("""
+                                                        QProgressBar{
+                                                            border: 2px solid grey;
+                                                            border-radius: 5px;
+                                                            text-align: center
+                                                        }
+
+                                                        QProgressBar::chunk {
+                                                            background-color: green;
+                                                        }
+                                                        """)
+            else:
+                self.battery_progressbar.setStyleSheet("""
+                                                        QProgressBar{
+                                                            border: 2px solid grey;
+                                                            border-radius: 5px;
+                                                            text-align: center
+                                                        }
+
+                                                        QProgressBar::chunk {
+                                                            background-color: orange;
+                                                        }
+                                                        """)
             
 
 if __name__ == "__main__":
